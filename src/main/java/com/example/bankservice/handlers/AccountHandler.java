d5239639c9f8a4875e5c4945f5686ac06656eacf
Suman Bandopadhyay <sumanbando006@gmail.com>
2020-08-07 21:52:48 +0530

    Code restructuring

--- src/main/java/com/example/bankservice/handlers/AccountHandler.java
+++ src/main/java/com/example/bankservice/handlers/AccountHandler.java
@@ -7,6 +7,7 @@ import com.example.bankservice.model.Account;
 import com.example.bankservice.model.AccountType;
 import com.mongodb.reactivestreams.client.ClientSession;
 import com.mongodb.reactivestreams.client.MongoClient;
+import jdk.jfr.ContentType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
@@ -23,6 +24,8 @@ import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 
+import static org.springframework.web.reactive.function.server.ServerResponse.*;
+
 @Slf4j
 @Component
 public class AccountHandler {
@@ -34,20 +37,18 @@ public class AccountHandler {
     private MongoClient client;
 
     public Mono<ServerResponse> getAllAccounts(ServerRequest serverRequest) {
-        Flux<Account> accounts = template.findAll(Account.class);
-        return ServerResponse
-                .ok()
+        var accounts = template.findAll(Account.class);
+        return ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(accounts, Account.class);
     }
 
     @Transactional
     public Mono<ServerResponse> createAccount(ServerRequest request) {
-        Mono<Account> account = request.bodyToMono(Account.class);
-        Flux<Account> savedAccount = template.inTransaction().execute(reactiveMongoOperations -> reactiveMongoOperations.insert(account),ClientSession::startTransaction);
+        var account = request.bodyToMono(Account.class);
+        var savedAccount = template.inTransaction().execute(reactiveMongoOperations -> reactiveMongoOperations.save(account),ClientSession::startTransaction);
 
-        return ServerResponse
-                .ok()
+        return ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(savedAccount, Account.class);
     }
@@ -55,74 +56,57 @@ public class AccountHandler {
     @Transactional
     public Mono<ServerResponse> transferAmount(ServerRequest request) {
         // Request
-        Mono<AccountTransferRequest> accountTransferRequest = request.bodyToMono(AccountTransferRequest.class);
+        var accountTransferRequest = request.bodyToMono(AccountTransferRequest.class);
         // From and To Account
         return accountTransferRequest.flatMap(req -> {
-            Mono<Account> fromAccount = template.findById(req.getFromAccountId(), Account.class);
-            Mono<Account> toAccount = template.findById(req.getToAccountId(), Account.class);
-            return fromAccount.flatMap(fAcc -> toAccount.flatMap(tAcc -> {
-                log.info("From Account : " + fAcc.getUser().getId());
-                log.info("To Account : " + tAcc.getUser().getId());
-                // Check if the transfer is between accounts of same user
-                if (!fAcc.getUser().getId().equals(tAcc.getUser().getId())) {
-                    // Check if transfer amount is available in source account
-                    if (req.getAmount() <= fAcc.getAmount()) {
+            var fromAccount = template.findById(req.getFromAccountId(), Account.class);
+            var toAccount = template.findById(req.getToAccountId(), Account.class);
+            return fromAccount
+                    .flatMap(fAcc -> toAccount.flatMap(tAcc -> {
+                        log.info("From Account : " + fAcc.getUser().getId());
+                        log.info("To Account : " + tAcc.getUser().getId());
+                        // Check if the transfer is between accounts of same user
+                        if (fAcc.getUser().getId().equals(tAcc.getUser().getId())) return failureServerResponse(HttpStatus.BAD_REQUEST, "Transferring to your own account is not allowed");
+                        // Check if transfer amount is available in source account
+                        if (req.getAmount() >= fAcc.getAmount()) return failureServerResponse(HttpStatus.BAD_REQUEST, "Required amount not available");
                         // Check if the destination amount type is Basic Savings and the total balance do not cross 50000
                         if (tAcc.getAccountType().equals(AccountType.BasicSavings)
-                                && tAcc.getAmount()+req.getAmount() > 50000) {
-                            AccountTransferFailure failure = AccountTransferFailure.builder()
-                                    .errorCode(HttpStatus.BAD_REQUEST)
-                                    .errorMessage("Basic Savings account cannot have more than 50000")
-                                    .build();
-                            return ServerResponse
-                                    .badRequest()
-                                    .contentType(MediaType.APPLICATION_JSON)
-                                    .body(Mono.just(failure), AccountTransferFailure.class);
-                        }
+                                && tAcc.getAmount()+req.getAmount() > 50000) return failureServerResponse(HttpStatus.BAD_REQUEST, "Basic Savings account cannot have more than 50000");
                         // Updating Source and Destination account
                         fAcc.setAmount(fAcc.getAmount()-req.getAmount());
                         tAcc.setAmount(tAcc.getAmount()+req.getAmount());
 
-                        Flux<AccountTransferResponse> response = template.inTransaction()
+                        var response = template.inTransaction()
                                 .execute(reactiveMongoOperations -> reactiveMongoOperations.save(fAcc)
-                                        .flatMap(aaaa -> reactiveMongoOperations.save(tAcc))
+                                        .then(reactiveMongoOperations.save(tAcc).map(aa -> reactiveMongoOperations))
                                         .then(reactiveMongoOperations.find(
                                                 new Query(Criteria.where("user").is(tAcc.getUser())),
                                                 Account.class)
                                                 .map(Account::getAmount)
                                                 .reduce(0.0, (x1, x2) -> x1 + x2)
                                                 .flatMap(totalDestAmount -> {
-                                                    AccountTransferResponse res = AccountTransferResponse.builder()
+                                                    var res = AccountTransferResponse.builder()
                                                             .newSourceBalance(fAcc.getAmount())
                                                             .totalDestBalance(totalDestAmount)
                                                             .timestamp(LocalDateTime.now())
                                                             .build();
                                                     return Mono.just(res);
                                                 })));
-                        return ServerResponse
-                                .ok()
+                        return ok()
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .body(response, AccountTransferResponse.class);
-                    }
-                    AccountTransferFailure failure = AccountTransferFailure.builder()
-                            .errorCode(HttpStatus.BAD_REQUEST)
-                            .errorMessage("Required amount not available")
-                            .build();
-                    return ServerResponse
-                            .badRequest()
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .body(Mono.just(failure), AccountTransferFailure.class);
-                }
-                AccountTransferFailure failure = AccountTransferFailure.builder()
-                        .errorCode(HttpStatus.BAD_REQUEST)
-                        .errorMessage("Transferring to your own account is not allowed")
-                        .build();
-                return ServerResponse
-                        .badRequest()
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .body(Mono.just(failure), AccountTransferFailure.class);
             }));
         });
+    }
+
+    private Mono<ServerResponse> failureServerResponse(HttpStatus httpStatus, String message) {
+        var failure = AccountTransferFailure.builder()
+                .errorCode(httpStatus)
+                .errorMessage(message)
+                .build();
+        return badRequest()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(failure), AccountTransferFailure.class);
     }
 
 }
